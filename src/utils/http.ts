c68075f7f1b7ffaffc35c1348af68f6783c3f44a
doooghan <doooghan@foxmail.com>
2023-04-23 17:43:44 +0800

    feat: fetch 封装

---
+++ src/utils/http.ts
@@ -0,0 +1,47 @@
+import qs from "qs";
+import * as auth from "@/auth-provider";
+
+interface Config extends RequestInit {
+  data?: object;
+  token?: string;
+}
+
+const apiUrl = import.meta.env.VITE_APP_API_URL;
+
+export const http = (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+
+  // axios 和 fetch 表现不一样，axios会在返回是非 2xx 的情况下抛出异常
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登陆" });
+      }
+
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
+};
