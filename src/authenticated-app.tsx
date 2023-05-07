3fb5c77cdeb0ac15fe286caed56ae13002359019
doooghan <doooghan@foxmail.com>
2023-05-07 18:07:47 +0800

    feat: 学习 flex 和 grid 布局

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { useAuth } from "./context/auth-context";
 import { ProjectListScreen } from "./screens/project-list";
 
@@ -5,10 +6,42 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
 
   return (
-    <div>
-      <button onClick={() => logout()}>登出</button>
+    <Container>
+      <PageHeader>
+        <HeaderLeft>
+          <h3>logo</h3>
+          <h3>测试1</h3>
+          <h3>占位2</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={() => logout()}>登出</button>
+        </HeaderRight>
+      </PageHeader>
 
-      <ProjectListScreen />
-    </div>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem calc(100vh - 6rem);
+`;
+
+const PageHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const HeaderLeft = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  /* height: calc(100vh - 6rem); */
+`;
