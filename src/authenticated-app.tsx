5a91de28f6ddc12ddb12a7fc284c9ffe261a4bba
doooghan <doooghan@foxmail.com>
2023-05-08 18:05:02 +0800

    feat: 优化项目列表页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,20 +2,32 @@ import styled from "@emotion/styled";
 import { useAuth } from "./context/auth-context";
 import { ProjectListScreen } from "./screens/project-list";
 import { Row } from "@/components/lib";
+import { ReactComponent as SoftwareLogo } from "@/assets/software-logo.svg";
+import { Dropdown, Menu } from "antd";
 
 export const AuthenticatedApp = () => {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
 
   return (
     <Container>
       <PageHeader between={true}>
         <HeaderLeft gap={true}>
-          <h3>logo</h3>
+          <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
           <h3>测试1</h3>
           <h3>占位2</h3>
         </HeaderLeft>
         <HeaderRight>
-          <button onClick={() => logout()}>登出</button>
+          <Dropdown
+            overlay={
+              <Menu>
+                <Menu.Item key={"logout"}>
+                  <a onClick={logout}>登出</a>
+                </Menu.Item>
+              </Menu>
+            }
+          >
+            <a onClick={(e) => e.preventDefault()}>hi! {user?.name}</a>
+          </Dropdown>
         </HeaderRight>
       </PageHeader>
 
@@ -31,7 +43,11 @@ const Container = styled.div`
   grid-template-rows: 6rem calc(100vh - 6rem);
 `;
 
-const PageHeader = styled(Row)``;
+const PageHeader = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
 const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
 
