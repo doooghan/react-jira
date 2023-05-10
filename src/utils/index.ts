5e4196b8af519e68ce1fa53a9586378e68545279
doooghan <doooghan@foxmail.com>
2023-05-10 23:15:29 +0800

    fix: 代码 bug 修复

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,13 +4,14 @@ import { useEffect } from "react";
 // 0 是一个有效值
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
-export const cleanObject = (object: object) => {
+export const isVoid = (value: unknown) =>
+  value === undefined || value === null || value === "";
+
+export const cleanObject = (object: { [key: string]: unknown }) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
-    // @ts-ignore
     const value = result[key];
-    if (isFalsy(value)) {
-      // @ts-ignore
+    if (isVoid(value)) {
       delete result[key];
     }
   });
