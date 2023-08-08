5ecc509db7e0cc7ded2ed6e9aebed2b7202a2883
doooghan <doooghan@foxmail.com>
2023-08-08 22:05:09 +0800

    style: 优化看板样式

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { ScreenContainer } from "components/lib";
 import React from "react";
 import { useDocumentTitle } from "utils";
 import { useKanbans } from "utils/kanban";
@@ -13,7 +14,7 @@ export const KanbanScreen = () => {
   const { data: kanbans } = useKanbans(useKanbansSearchParams());
 
   return (
-    <>
+    <ScreenContainer>
       <h1>{currentData?.name}看板</h1>
       <SearchPanel></SearchPanel>
       <ColumnsContainer>
@@ -21,12 +22,12 @@ export const KanbanScreen = () => {
           <KanbanColumn kanban={kanban} key={kanban.id} />
         ))}
       </ColumnsContainer>
-    </>
+    </ScreenContainer>
   );
 };
 
 const ColumnsContainer = styled.div`
   display: flex;
-  overflow: hidden;
-  margin-right: 2rem;
+  overflow-x: scroll;
+  flex: 1 0 auto;
 `;
