70d96280dfc6906f0572b3f302bd1bc03385dd6e
doooghan <doooghan@foxmail.com>
2023-08-09 23:28:41 +0800

    feat: 自己的mark

--- src/components/mark.tsx
+++ src/components/mark.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 
+export const Mark = ({ name, keyword }: { name: string; keyword: string }) => {
+  if (!keyword) {
+    return <>{name}</>;
+  }
+  const arr = name.split(keyword);
+  const arr2 = arr.map((str) => <span>{str}</span>);
+  // const arr3 = arr2.join(<span>{keyword}</span>)
+  return (
+    <>
+      {arr2.map((item, index) => (
+        <>
+          {item}
+          {index === arr.length - 1 ? null : (
+            <span style={{ color: "#257AFD" }}>{keyword}</span>
+          )}
+        </>
+      ))}
+    </>
+  );
+};
 // export const Mark = ({ name, keyword }: { name: string; keyword: string }) => {
 //   if (!keyword) {
 //     return <>{name}</>;
 //   }
 //   const arr = name.split(keyword);
-//   const arr2 = arr.map((str) => <span>{str}</span>)
-//   const arr3 = arr2.join(<span>{keyword}</span>)
 //   return (
 //     <>
 //       {arr.map((str, index) => (
@@ -20,21 +38,3 @@ import React from "react";
 //     </>
 //   );
 // };
-export const Mark = ({ name, keyword }: { name: string; keyword: string }) => {
-  if (!keyword) {
-    return <>{name}</>;
-  }
-  const arr = name.split(keyword);
-  return (
-    <>
-      {arr.map((str, index) => (
-        <span key={index}>
-          {str}
-          {index === arr.length - 1 ? null : (
-            <span style={{ color: "#257AFD" }}>{keyword}</span>
-          )}
-        </span>
-      ))}
-    </>
-  );
-};
