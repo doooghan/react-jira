3457ae8266cca58b71e16bd82bcad7eff55ea664
doooghan <doooghan@foxmail.com>
2023-08-10 23:00:26 +0800

    feat: task-dnd

--- src/screens/kanban/kanban-column.tsx
+++ src/screens/kanban/kanban-column.tsx
@@ -16,6 +16,7 @@ import { Task } from "types/task";
 import { Mark } from "components/mark";
 import { useDeleteKanban } from "utils/kanban";
 import { Row } from "components/lib";
+import { Drag, Drop, DropChild } from "components/drag-and-drop";
 
 const TaskTypeIcon = ({ id }: { id: number }) => {
   const { data: taskTypes } = useTaskTypes();
@@ -60,9 +61,21 @@ export const KanbanColumn = React.forwardRef<
         <More kanban={kanban} key={kanban.id}></More>
       </Row>
       <TasksContainer>
-        {tasks?.map((task) => (
-          <TaskCard task={task} key={task.id} />
-        ))}
+        <Drop
+          type={"ROW"}
+          direction={"vertical"}
+          droppableId={String(kanban.id)}
+        >
+          <DropChild>
+            {tasks?.map((task, index) => (
+              <Drag key={task.id} index={index} draggableId={"task" + task.id}>
+                <div>
+                  <TaskCard task={task} key={task.id} />
+                </div>
+              </Drag>
+            ))}
+          </DropChild>
+        </Drop>
         <CreateTask kanbanId={kanban.id}></CreateTask>
       </TasksContainer>
     </Container>
